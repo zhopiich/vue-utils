740fbb7d43a8b64650a45dcd6140830226e1afd9
zhopiich <zhoping9449@gmail.com>
2026-03-25 16:20:04 +0800

    feat: add unrefElement

---
+++ src/composables/unrefElement/unrefElement.ts
@@ -0,0 +1,24 @@
+import type { ComponentPublicInstance, MaybeRef, MaybeRefOrGetter } from 'vue'
+
+export type VueInstance = ComponentPublicInstance
+export type MaybeElement = VueInstance | HTMLElement | SVGElement | undefined | null
+
+export type MaybeElementRef<T extends MaybeElement = MaybeElement> = MaybeRef<T>
+export type MaybeComputedElementRef<T extends MaybeElement = MaybeElement> = MaybeRefOrGetter<T>
+
+export type UnRefElementReturn<T extends MaybeElement = MaybeElement>
+  = T extends HTMLElement ? HTMLElement
+    : T extends SVGElement ? SVGElement
+      : T extends VueInstance ? T['$el']
+        : null | undefined
+
+export function isVueInstance(el: unknown): el is VueInstance {
+  return el != null && typeof el === 'object' && '$el' in el
+}
+
+export function unrefElement<T extends MaybeElement>(
+  elRef: MaybeComputedElementRef<T>,
+): UnRefElementReturn<T> {
+  const plainEl = toValue(elRef)
+  return (isVueInstance(plainEl) ? plainEl.$el : plainEl) as UnRefElementReturn<T>
+}
